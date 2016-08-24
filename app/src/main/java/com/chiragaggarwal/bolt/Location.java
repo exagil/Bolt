1f60ef7c89e36832b0e34dfbafa0fa4750c0af15
Chirag <me@chi6rag.net>
2016-08-24 20:46:03 +0530

    Location knows about speed and accuracy

--- app/src/main/java/com/chiragaggarwal/bolt/Location.java
+++ app/src/main/java/com/chiragaggarwal/bolt/Location.java
@@ -3,19 +3,34 @@ package com.chiragaggarwal.bolt;
 public class Location {
     private final double latitude;
     private final double longitude;
+    private final boolean hasAccuracy;
+    private final int accuracy;
+    private final boolean hasSpeed;
+    private final float speed;
 
-    public Location(double latitude, double longitude) {
+    public Location(double latitude, double longitude, boolean hasAccuracy, int accuracy, boolean hasSpeed, float speed) {
         this.latitude = latitude;
         this.longitude = longitude;
+        this.hasAccuracy = hasAccuracy;
+        this.accuracy = accuracy;
+        this.hasSpeed = hasSpeed;
+        this.speed = speed;
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || !(o instanceof Location)) return false;
-        Location thatLocation = (Location) o;
-        return this.latitude == thatLocation.latitude &&
-                this.longitude == thatLocation.longitude;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Location location = (Location) o;
+
+        if (Double.compare(location.latitude, latitude) != 0) return false;
+        if (Double.compare(location.longitude, longitude) != 0) return false;
+        if (hasAccuracy != location.hasAccuracy) return false;
+        if (accuracy != location.accuracy) return false;
+        if (hasSpeed != location.hasSpeed) return false;
+        return Float.compare(location.speed, speed) == 0;
+
     }
 
     @Override
@@ -26,6 +41,10 @@ public class Location {
         result = (int) (temp ^ (temp >>> 32));
         temp = Double.doubleToLongBits(longitude);
         result = 31 * result + (int) (temp ^ (temp >>> 32));
+        result = 31 * result + (hasAccuracy ? 1 : 0);
+        result = 31 * result + accuracy;
+        result = 31 * result + (hasSpeed ? 1 : 0);
+        result = 31 * result + (speed != +0.0f ? Float.floatToIntBits(speed) : 0);
         return result;
     }
 
@@ -33,6 +52,8 @@ public class Location {
         android.location.Location nativeLocation = new android.location.Location("");
         nativeLocation.setLatitude(this.latitude);
         nativeLocation.setLongitude(this.longitude);
+        nativeLocation.setSpeed(speed);
+        nativeLocation.setAccuracy(accuracy);
         return nativeLocation;
     }
 }
--- app/src/test/java/com/chiragaggarwal/bolt/LocationTest.java
+++ app/src/test/java/com/chiragaggarwal/bolt/LocationTest.java
@@ -9,40 +9,40 @@ import static junit.framework.Assert.assertTrue;
 public class LocationTest {
     @Test
     public void testThatALocationIsNotEqualToNull() {
-        Location location = new Location(12.9611d, 77.6472d);
+        Location location = new Location(12.9611d, 77.6472d, true, 16, true, 3.16f);
         assertFalse(location.equals(null));
     }
 
     @Test
     public void testThatALocationIsEqualToItself() {
-        Location location = new Location(12.9611d, 77.6472d);
+        Location location = new Location(12.9611d, 77.6472d, true, 16, true, 3.16f);
         assertTrue(location.equals(location));
     }
 
     @Test
-    public void testThatALocationIsEqualToAnotherLocationWithSameCoordinates() {
-        Location location = new Location(12.9611d, 77.6472d);
-        Location anotherLocation = new Location(12.9611d, 77.6472d);
+    public void testThatALocationIsEqualToAnotherLocationWithSameStates() {
+        Location location = new Location(12.9611d, 77.6472d, true, 16, true, 3.16f);
+        Location anotherLocation = new Location(12.9611d, 77.6472d, true, 16, true, 3.16f);
         assertTrue(location.equals(anotherLocation));
     }
 
     @Test
-    public void testThatALocationIsNotEqualToAnotherLocationWithDifferentCoordinates() {
-        Location location = new Location(12.9611d, 77.6472d);
-        Location anotherLocation = new Location(13.9611d, 78.6472d);
+    public void testThatALocationIsNotEqualToAnotherLocationWithDifferentStates() {
+        Location location = new Location(12.9611d, 77.6472d, true, 16, true, 3.16f);
+        Location anotherLocation = new Location(13.9611d, 78.6472d, false, 5, false, 6.13f);
         assertFalse(location.equals(anotherLocation));
     }
 
     @Test
     public void testThatALocationIsNotEqualToAnythingOtherThanALocation() {
-        Location location = new Location(12.9611d, 77.6472d);
+        Location location = new Location(12.9611d, 77.6472d, true, 16, true, 3.16f);
         assertFalse(location.equals(new Object()));
     }
 
     @Test
     public void testThatTwoSameLocationsHaveTheSameHashCode() {
-        Location location = new Location(12.9611d, 77.6472d);
-        Location anotherLocation = new Location(12.9611d, 77.6472d);
+        Location location = new Location(12.9611d, 77.6472d, true, 16, true, 3.16f);
+        Location anotherLocation = new Location(12.9611d, 77.6472d, true, 16, true, 3.16f);
         assertEquals(location.hashCode(), anotherLocation.hashCode());
     }
 }
