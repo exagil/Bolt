8cadb871e2ac65f9d07080ab703cca86ded7654a
Chirag <me@chi6rag.net>
2016-08-24 06:47:31 +0530

    Adding LocationApiClient for continuously listening to the change in user's location

--- app/src/main/java/com/chiragaggarwal/bolt/LocationApiClient.java
+++ app/src/main/java/com/chiragaggarwal/bolt/LocationApiClient.java
@@ -1,14 +1,59 @@
 package com.chiragaggarwal.bolt;
 
-public class LocationApiClient {
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+public class LocationApiClient implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
+
+    private final GoogleApiClient googleApiClient;
     private LocationChangeListener locationChangeListener;
 
-    public LocationApiClient(LocationChangeListener locationChangeListener) {
+    public LocationApiClient(Context context, LocationChangeListener locationChangeListener) {
         this.locationChangeListener = locationChangeListener;
+        googleApiClient = buildGoogleApiClientForLocation(context);
     }
 
+    public void connect() {
+        googleApiClient.connect();
+    }
+
+    @Override
     public void onLocationChanged(android.location.Location nativeLocation) {
         Location location = new Location(nativeLocation.getLatitude(), nativeLocation.getLongitude());
         locationChangeListener.onLocationChanged(location);
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        LocationRequest locationRequest = new LocationRequest();
+        LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+    }
+
+    @NonNull
+    private GoogleApiClient buildGoogleApiClientForLocation(Context context) {
+        return new GoogleApiClient.Builder(context)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
     }
 }
--- app/src/test/java/com/chiragaggarwal/bolt/LocationApiClientTest.java
+++ app/src/test/java/com/chiragaggarwal/bolt/LocationApiClientTest.java
@@ -1,21 +1,24 @@
 package com.chiragaggarwal.bolt;
 
 
+import android.content.Context;
+
 import org.junit.Test;
-import org.mockito.Mockito;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class LocationApiClientTest {
     @Test
     public void testShouldKnowIfALocationChanged() {
         LocationChangeListener locationChangeListener = mock(LocationChangeListener.class);
-        LocationApiClient locationApiClient = new LocationApiClient(locationChangeListener);
+        Context context = mock(Context.class);
+        LocationApiClient locationApiClient = new LocationApiClient(context, locationChangeListener);
         Location location = new Location(12.9611d, 77.6472d);
         android.location.Location nativeLocation = mock(android.location.Location.class);
-        Mockito.when(nativeLocation.getLatitude()).thenReturn(12.9611d);
-        Mockito.when(nativeLocation.getLongitude()).thenReturn(77.6472d);
+        when(nativeLocation.getLatitude()).thenReturn(12.9611d);
+        when(nativeLocation.getLongitude()).thenReturn(77.6472d);
         locationApiClient.onLocationChanged(nativeLocation);
         verify(locationChangeListener).onLocationChanged(location);
     }
