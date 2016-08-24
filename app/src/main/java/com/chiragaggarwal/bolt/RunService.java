82eaa1a965a0c1566a874209a7e10e50cd10b129
Chirag <me@chi6rag.net>
2016-08-24 21:22:18 +0530

    Adding elementary implementation of RunService

---
+++ app/src/main/java/com/chiragaggarwal/bolt/RunService.java
@@ -0,0 +1,47 @@
+package com.chiragaggarwal.bolt;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import javax.inject.Inject;
+
+public class RunService extends Service implements LocationChangeListener {
+    @Inject
+    public GoogleApiClient googleApiClient;
+    private LocationApiClient locationApiClient;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        ((BoltApplication) getApplicationContext()).getBoltComponent().inject(this);
+        locationApiClient = new LocationApiClient(googleApiClient, this);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        locationApiClient.connect();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return new RunServiceBinder();
+    }
+
+    @Override
+    public void onFetchAccurateLocation(Location location) {
+        
+    }
+
+    public class RunServiceBinder extends Binder {
+        public RunService getService() {
+            return RunService.this;
+        }
+    }
+}
