773a572ac25b7a8ac87712d95939b7d363d76e83
Chirag <me@chi6rag.net>
2016-10-01 23:35:55 +0530

    Refactoring UserLocations

--- app/src/main/java/com/chiragaggarwal/bolt/location/UserLocations.java
+++ app/src/main/java/com/chiragaggarwal/bolt/location/UserLocations.java
@@ -1,31 +1,26 @@
 package com.chiragaggarwal.bolt.location;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class UserLocations {
-    private List<UserLocation> userLocationList;
     private UserLocation lastVisitedUserLocation;
     private float totalDistanceInKilometers;
     private float currentPaceInKilometersPerHour;
-
-    public UserLocations() {
-        this.userLocationList = new ArrayList<>();
-    }
 
     public float totalDistanceInKilometers() {
         return totalDistanceInKilometers;
     }
 
     public void add(UserLocation userLocation) {
-        if (lastVisitedUserLocation != null)
+        if (hasUserMoved())
             totalDistanceInKilometers += lastVisitedUserLocation.distanceInKilometersTo(userLocation);
-        userLocationList.add(userLocation);
         lastVisitedUserLocation = userLocation;
         currentPaceInKilometersPerHour = userLocation.speedInKilometersPerHour();
     }
 
     public float currentPaceInKilometersPerHour() {
         return currentPaceInKilometersPerHour;
+    }
+
+    private boolean hasUserMoved() {
+        return lastVisitedUserLocation != null;
     }
 }
