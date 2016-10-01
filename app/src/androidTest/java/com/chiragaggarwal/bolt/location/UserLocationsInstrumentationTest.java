00ea9ee11eb1cf1aedf0f5ec510b3d38bcdc4219
Chirag <me@chi6rag.net>
2016-10-01 23:18:18 +0530

    UserLocations knows the total distance the user has travelled over the activity

---
+++ app/src/androidTest/java/com/chiragaggarwal/bolt/location/UserLocationsInstrumentationTest.java
@@ -0,0 +1,26 @@
+package com.chiragaggarwal.bolt.location;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class UserLocationsInstrumentationTest {
+    @Test
+    public void testThatItKnowsTheTotalDistanceTravelledInKilometersWhenItHasAlreadyVisitedACoupleOfLocations() {
+        UserLocations userLocations = new UserLocations();
+        UserLocation userLocation = new UserLocation(12.9611d, 77.6472d, true, 19, true, 11.98765f);
+        userLocations.add(userLocation);
+        UserLocation secondUserLocation = new UserLocation(12.9612d, 77.6473d, true, 19, true, 11.98765f);
+        userLocations.add(secondUserLocation);
+        Assert.assertEquals(0.015495687F, userLocations.totalDistanceInKilometers());
+    }
+
+    @Test
+    public void testThatItKnowsTheTotalDistanceTravelledInKilometersWhenItHasAlreadyVisitedAMultipleLocations() {
+        UserLocations userLocations = new UserLocations();
+        userLocations.add(new UserLocation(-6.1805294D, 106.8088036D, true, 1, true, 10));
+        userLocations.add(new UserLocation(-6.1805228D, 106.8088433D, true, 1, true, 10));
+        userLocations.add(new UserLocation(-6.1805211D, 106.8088532D, true, 1, true, 10));
+        Assert.assertEquals(0.0055657863F, userLocations.totalDistanceInKilometers());
+    }
+}
--- app/src/main/java/com/chiragaggarwal/bolt/location/UserLocations.java
+++ app/src/main/java/com/chiragaggarwal/bolt/location/UserLocations.java
@@ -1,11 +1,25 @@
 package com.chiragaggarwal.bolt.location;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserLocations {
+    private List<UserLocation> userLocationList;
+    private UserLocation lastVisitedUserLocation;
+    private float totalDistanceInKilometers;
+
+    public UserLocations() {
+        this.userLocationList = new ArrayList<>();
+    }
+
     public float totalDistanceInKilometers() {
-        return 0;
+        return totalDistanceInKilometers;
     }
 
     public void add(UserLocation userLocation) {
-
+        if (lastVisitedUserLocation != null)
+            totalDistanceInKilometers += lastVisitedUserLocation.distanceInKilometersTo(userLocation);
+        userLocationList.add(userLocation);
+        lastVisitedUserLocation = userLocation;
     }
 }
