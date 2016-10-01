b3ea195e144b28e4dfa2ab58bc2605e031b2580f
Chirag <me@chi6rag.net>
2016-10-01 23:02:51 +0530

    Distance in kilometers is zero when user has only travelled one location

--- app/src/main/java/com/chiragaggarwal/bolt/location/UserLocations.java
+++ app/src/main/java/com/chiragaggarwal/bolt/location/UserLocations.java
@@ -4,4 +4,8 @@ public class UserLocations {
     public float totalDistanceInKilometers() {
         return 0;
     }
+
+    public void add(UserLocation userLocation) {
+
+    }
 }
--- app/src/test/java/com/chiragaggarwal/bolt/location/UserLocationsTest.java
+++ app/src/test/java/com/chiragaggarwal/bolt/location/UserLocationsTest.java
@@ -10,4 +10,12 @@ public class UserLocationsTest {
         UserLocations userLocations = new UserLocations();
         Assert.assertEquals(0F, userLocations.totalDistanceInKilometers());
     }
+
+    @Test
+    public void testThatDistanceInKilometersIsZeroWhenUserHasOnlyTravelledOneLocation() {
+        UserLocations userLocations = new UserLocations();
+        UserLocation userLocation = new UserLocation(12.9611d, 77.6472d, true, 16, true, 3.16f);
+        userLocations.add(userLocation);
+        Assert.assertEquals(0F, userLocations.totalDistanceInKilometers());
+    }
 }
