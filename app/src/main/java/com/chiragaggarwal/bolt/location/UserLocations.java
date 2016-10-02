44baac56f234b97de12f55d8ac8819fa4d2c4c81
Chirag <me@chi6rag.net>
2016-10-02 10:50:59 +0530

    Adding equality and hashcode to UserLocations

--- app/src/main/java/com/chiragaggarwal/bolt/location/UserLocations.java
+++ app/src/main/java/com/chiragaggarwal/bolt/location/UserLocations.java
@@ -23,4 +23,27 @@ public class UserLocations {
     private boolean hasUserMoved() {
         return lastVisitedUserLocation != null;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UserLocations that = (UserLocations) o;
+
+        if (Float.compare(that.totalDistanceInKilometers, totalDistanceInKilometers) != 0)
+            return false;
+        if (Float.compare(that.currentPaceInKilometersPerHour, currentPaceInKilometersPerHour) != 0)
+            return false;
+        return lastVisitedUserLocation != null ? lastVisitedUserLocation.equals(that.lastVisitedUserLocation) : that.lastVisitedUserLocation == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = lastVisitedUserLocation != null ? lastVisitedUserLocation.hashCode() : 0;
+        result = 31 * result + (totalDistanceInKilometers != +0.0f ? Float.floatToIntBits(totalDistanceInKilometers) : 0);
+        result = 31 * result + (currentPaceInKilometersPerHour != +0.0f ? Float.floatToIntBits(currentPaceInKilometersPerHour) : 0);
+        return result;
+    }
 }
--- app/src/test/java/com/chiragaggarwal/bolt/location/UserLocationsTest.java
+++ app/src/test/java/com/chiragaggarwal/bolt/location/UserLocationsTest.java
@@ -1,14 +1,26 @@
 package com.chiragaggarwal.bolt.location;
 
-import junit.framework.Assert;
-
+import org.junit.Before;
 import org.junit.Test;
 
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
+
 public class UserLocationsTest {
+    private UserLocations userLocations;
+
+    @Before
+    public void setup() {
+        userLocations = new UserLocations();
+        UserLocation userLocation = new UserLocation(12.9611d, 77.6472d, true, 16, true, 3.16f);
+        userLocations.add(userLocation);
+    }
+
     @Test
     public void testThatDistanceInKilometersIsZeroWhenUserHasNotVisitedAnyLocation() {
         UserLocations userLocations = new UserLocations();
-        Assert.assertEquals(0F, userLocations.totalDistanceInKilometers());
+        assertEquals(0F, userLocations.totalDistanceInKilometers());
     }
 
     @Test
@@ -16,13 +28,13 @@ public class UserLocationsTest {
         UserLocations userLocations = new UserLocations();
         UserLocation userLocation = new UserLocation(12.9611d, 77.6472d, true, 16, true, 3.16f);
         userLocations.add(userLocation);
-        Assert.assertEquals(0F, userLocations.totalDistanceInKilometers());
+        assertEquals(0F, userLocations.totalDistanceInKilometers());
     }
 
     @Test
     public void testThatCurrentPaceInKilometersPerHourIsZeroByDefault() {
         UserLocations userLocations = new UserLocations();
-        Assert.assertEquals(0F, userLocations.currentPaceInKilometersPerHour());
+        assertEquals(0F, userLocations.currentPaceInKilometersPerHour());
     }
 
     @Test
@@ -30,6 +42,43 @@ public class UserLocationsTest {
         UserLocations userLocations = new UserLocations();
         UserLocation userLocation = new UserLocation(12.9611d, 77.6472d, true, 16, true, 3.16F);
         userLocations.add(userLocation);
-        Assert.assertEquals(11.376F, userLocations.currentPaceInKilometersPerHour());
+        assertEquals(11.376F, userLocations.currentPaceInKilometersPerHour());
+    }
+
+    @Test
+    public void testThatUserLocationsAreNotEqualToNull() {
+        assertFalse(userLocations.equals(null));
+    }
+
+    @Test
+    public void testThatUserLocationsAreEqualToThemselves() {
+        assertTrue(userLocations.equals(userLocations));
+    }
+
+    @Test
+    public void testThatUserLocationsAreEqualToAnotherUserLocationsWithSameStates() {
+        UserLocations anotherUserLocations = new UserLocations();
+        UserLocation anotherUserLocation = new UserLocation(12.9611d, 77.6472d, true, 16, true, 3.16f);
+        anotherUserLocations.add(anotherUserLocation);
+        assertTrue(this.userLocations.equals(anotherUserLocations));
+    }
+
+    @Test
+    public void testThatUserLocationsAreNotEqualToAnotherUserLocationsWithDifferentStates() {
+        UserLocations anotherUserLocations = new UserLocations();
+        assertFalse(userLocations.equals(anotherUserLocations));
+    }
+
+    @Test
+    public void testThatUserLocationsAreNotEqualToAnythingOtherThanUserLocations() {
+        assertFalse(userLocations.equals(new Object()));
+    }
+
+    @Test
+    public void testThatTwoSameLocationsHaveTheSameHashCode() {
+        UserLocation userLocation = new UserLocation(12.9611d, 77.6472d, true, 16, true, 3.16f);
+        UserLocations anotherUserLocations = new UserLocations();
+        anotherUserLocations.add(userLocation);
+        assertEquals(this.userLocations.hashCode(), anotherUserLocations.hashCode());
     }
 }
