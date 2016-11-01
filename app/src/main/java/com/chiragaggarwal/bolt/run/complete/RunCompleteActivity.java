e6b9c4117a941777c8bf42a147f131dcb5d6a64c
Chirag <me@chi6rag.net>
2016-11-01 19:39:25 +0530

    Cleaning up bloated style resources in xml

--- app/src/main/java/com/chiragaggarwal/bolt/run/complete/RunCompleteActivity.java
+++ app/src/main/java/com/chiragaggarwal/bolt/run/complete/RunCompleteActivity.java
@@ -5,13 +5,11 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import com.chiragaggarwal.bolt.R;
-import com.chiragaggarwal.bolt.location.UserLocations;
 
 public class RunCompleteActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_run_complete);
-        UserLocations userLocations = getIntent().getParcelableExtra(UserLocations.TAG);
     }
 }
