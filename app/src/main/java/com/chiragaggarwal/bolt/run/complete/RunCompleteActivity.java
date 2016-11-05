bb62da069433065324c3fcd68f3eeadfdffc6cb1
Chirag <me@chi6rag.net>
2016-11-05 09:14:59 +0530

    Adding elapsed time in run complete toolbar

--- app/src/main/java/com/chiragaggarwal/bolt/run/complete/RunCompleteActivity.java
+++ app/src/main/java/com/chiragaggarwal/bolt/run/complete/RunCompleteActivity.java
@@ -3,18 +3,28 @@ package com.chiragaggarwal.bolt.run.complete;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.chiragaggarwal.bolt.R;
 import com.chiragaggarwal.bolt.run.RunViewModel;
 import com.chiragaggarwal.bolt.timer.ElapsedTime;
 
 public class RunCompleteActivity extends AppCompatActivity {
+    private RunViewModel runViewModel;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_run_complete);
         ElapsedTime elapsedTime = getIntent().getParcelableExtra(ElapsedTime.TAG);
-        RunViewModel runViewModel = new RunViewModel(getResources());
+        runViewModel = new RunViewModel(getResources());
         runViewModel.setElapsedTime(elapsedTime);
+        setupToolbar();
+    }
+
+    private void setupToolbar() {
+        Toolbar toolbarRunComplete = (Toolbar) findViewById(R.id.run_complete_toolbar);
+        setSupportActionBar(toolbarRunComplete);
+        getSupportActionBar().setTitle(runViewModel.getElapsedTime());
     }
 }
