2dc99f5b7beea184b3f2b2087b8a7b4bdaa2eba2
Chirag <me@chi6rag.net>
2016-11-06 13:19:20 +0530

    Run knows about its persistable form

--- app/src/androidTest/java/com/chiragaggarwal/bolt/run/RunInstrumentationTest.java
+++ app/src/androidTest/java/com/chiragaggarwal/bolt/run/RunInstrumentationTest.java
@@ -6,11 +6,10 @@ import android.support.test.runner.AndroidJUnit4;
 import com.chiragaggarwal.bolt.location.UserLocations;
 import com.chiragaggarwal.bolt.run.persistance.BoltDatabaseSchema;
 
+import junit.framework.Assert;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import io.reactivex.Observable;
-import io.reactivex.observers.TestObserver;
 
 @RunWith(AndroidJUnit4.class)
 public class RunInstrumentationTest {
@@ -18,12 +17,10 @@ public class RunInstrumentationTest {
     public void testThatRunKnowsAboutItsPersistableForm() {
         UserLocations userLocations = null;
         Run run = new Run(5, "valid note", userLocations);
-        Observable<ContentValues> runObservable = run.persistable();
-        TestObserver<ContentValues> observer = new TestObserver<>();
-        runObservable.subscribe(observer);
+        ContentValues runContentValues = run.persistable();
         ContentValues expectedRunContentValue = new ContentValues();
         expectedRunContentValue.put(BoltDatabaseSchema.RunSchema.RATING, 5);
         expectedRunContentValue.put(BoltDatabaseSchema.RunSchema.NOTE, "valid note");
-        observer.assertResult(expectedRunContentValue);
+        Assert.assertEquals(expectedRunContentValue, runContentValues);
     }
 }
--- app/src/main/java/com/chiragaggarwal/bolt/run/Run.java
+++ app/src/main/java/com/chiragaggarwal/bolt/run/Run.java
@@ -5,8 +5,6 @@ import android.content.ContentValues;
 import com.chiragaggarwal.bolt.location.UserLocations;
 import com.chiragaggarwal.bolt.run.persistance.BoltDatabaseSchema;
 
-import io.reactivex.Observable;
-
 public class Run {
     private final int rating;
     private final String note;
@@ -18,10 +16,10 @@ public class Run {
         this.userLocations = userLocations;
     }
 
-    public Observable<ContentValues> persistable() {
+    public ContentValues persistable() {
         ContentValues contentValues = new ContentValues();
         contentValues.put(BoltDatabaseSchema.RunSchema.NOTE, note);
         contentValues.put(BoltDatabaseSchema.RunSchema.RATING, rating);
-        return Observable.just(contentValues);
+        return contentValues;
     }
 }
