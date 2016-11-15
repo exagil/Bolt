cd6905277b9415ad095b6e4c6c256e7e4ad4978d
Chirag <me@chi6rag.net>
2016-11-15 08:14:34 +0530

    Adding a dummy widget

---
+++ app/src/main/java/com/chiragaggarwal/bolt/widget/RunsWidgetProvider.java
@@ -0,0 +1,17 @@
+package com.chiragaggarwal.bolt.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+public class RunsWidgetProvider extends AppWidgetProvider {
+    @Override
+    public void onEnabled(Context context) {
+        super.onEnabled(context);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+}
