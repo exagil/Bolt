47fb1d5549dfe470f7d0d0f472fde9f6d3ad624b
Chirag <me@chi6rag.net>
2016-11-19 12:35:44 +0530

    Opening run activity whenever clicked on empty widget layout

--- app/src/main/java/com/chiragaggarwal/bolt/widget/RunsWidgetProvider.java
+++ app/src/main/java/com/chiragaggarwal/bolt/widget/RunsWidgetProvider.java
@@ -1,18 +1,22 @@
 package com.chiragaggarwal.bolt.widget;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.widget.RemoteViews;
 
 import com.chiragaggarwal.bolt.R;
+import com.chiragaggarwal.bolt.run.ongoing.RunActivity;
 import com.chiragaggarwal.bolt.run.persistance.BoltDatabaseSchema;
 
 public class RunsWidgetProvider extends AppWidgetProvider {
+    private static final int CODE_START_RUN = 1;
     private final HandlerThread runsWidgetHandler;
     private final Handler workerQueue;
 
@@ -33,14 +37,19 @@ public class RunsWidgetProvider extends AppWidgetProvider {
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        for (int appWidgetIdIndex = 0; appWidgetIdIndex < appWidgetIds.length; appWidgetIdIndex++) {
-            int appWidgetId = appWidgetIds[appWidgetIdIndex];
+        for (int appWidgetId : appWidgetIds) {
             Intent intent = new Intent(context.getApplicationContext(), RunsWidgetService.class);
             RemoteViews runsRemoteViews = new RemoteViews(context.getPackageName(), R.layout.initial_layout);
             runsRemoteViews.setRemoteAdapter(R.id.widget_list_run_history, intent);
             runsRemoteViews.setEmptyView(R.id.widget_list_run_history, R.id.layout_empty);
+            runsRemoteViews.setOnClickPendingIntent(R.id.layout_empty, startRunPendingIntent(context));
             appWidgetManager.updateAppWidget(appWidgetId, runsRemoteViews);
         }
         super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+
+    private PendingIntent startRunPendingIntent(Context context) {
+        Intent intent = new Intent(context, RunActivity.class);
+        return PendingIntent.getActivity(context, CODE_START_RUN, intent, PendingIntent.FLAG_CANCEL_CURRENT, new Bundle());
     }
 }
