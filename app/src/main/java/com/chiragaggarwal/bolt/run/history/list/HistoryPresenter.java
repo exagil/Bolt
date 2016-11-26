27c55cada9963849098cc47a861de635f38acd4f
Chirag <me@chi6rag.net>
2016-11-26 16:29:48 +0530

    Getting runs history after 300 ms

--- app/src/main/java/com/chiragaggarwal/bolt/run/history/list/HistoryPresenter.java
+++ app/src/main/java/com/chiragaggarwal/bolt/run/history/list/HistoryPresenter.java
@@ -1,10 +1,12 @@
 package com.chiragaggarwal.bolt.run.history.list;
 
 import android.app.LoaderManager;
+import android.os.Handler;
 
 import com.chiragaggarwal.bolt.run.persistance.RunLocalStorage;
 
 public class HistoryPresenter {
+    public static final int DELAY = 300;
     private HistoryView historyView;
     private RunLocalStorage runLocalStorage;
 
@@ -14,6 +16,10 @@ public class HistoryPresenter {
     }
 
     public void loadRuns(LoaderManager loaderManager) {
+        new Handler().postDelayed(() -> loadRunsFromLocal(loaderManager), 300);
+    }
+
+    private void loadRunsFromLocal(LoaderManager loaderManager) {
         runLocalStorage.loadRuns(loaderManager, runs -> {
             if (runs.isEmpty()) {
                 historyView.onHistoryLoadFailed();
--- app/src/main/java/com/chiragaggarwal/bolt/run/persistance/RunLocalStorage.java
+++ app/src/main/java/com/chiragaggarwal/bolt/run/persistance/RunLocalStorage.java
@@ -8,9 +8,6 @@ import com.chiragaggarwal.bolt.common.OnSuccessCallback;
 import com.chiragaggarwal.bolt.run.Run;
 
 import java.util.List;
-import java.util.concurrent.TimeUnit;
-
-import io.reactivex.Observable;
 
 import static com.chiragaggarwal.bolt.run.persistance.BoltDatabaseSchema.RunSchema;
 
@@ -31,9 +28,7 @@ public class RunLocalStorage {
     }
 
     public void loadRuns(LoaderManager loaderManager, OnSuccessCallback<List<Run>> onSuccessCallback) {
-        Observable.timer(300, TimeUnit.MILLISECONDS)
-                .doOnEach(longNotification -> new RunsLoader(context, loaderManager, onSuccessCallback).load())
-                .subscribe();
+        new RunsLoader(context, loaderManager, onSuccessCallback).load();
 
     }
 
